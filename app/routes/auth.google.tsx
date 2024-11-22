6b455ef414541f1520b6cd666e8578ff8132e706
Nowe-melancholy <40950720+Nowe-melancholy@users.noreply.github.com>
2024-11-22 10:14:46 +0900

    a

--- app/routes/auth.google.tsx
+++ app/routes/auth.google.tsx
@@ -2,16 +2,23 @@ import type {
   ActionFunction,
   ActionFunctionArgs,
   LoaderFunction,
-} from '@remix-run/cloudflare';
-import { redirect } from '@remix-run/cloudflare';
-import { authenticator } from '~/services/auth.server';
+} from "@remix-run/cloudflare";
+import { redirect } from "@remix-run/cloudflare";
+import { authenticator } from "~/services/auth.server";
 
 export const action: ActionFunction = async ({
   request,
 }: ActionFunctionArgs) => {
-  return authenticator.authenticate('google', request);
+  console.log("&&&&&&&&&&&&", {
+    clientID: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    callbackURL: `${process.env.CLIENT_URL}/auth/google/callback`,
+    secrets: [process.env.SESSION_SECRET!],
+    secure: process.env.NODE_ENV === "production",
+  });
+  return authenticator.authenticate("google", request);
 };
 
 export const loader: LoaderFunction = async () => {
-  return redirect('/');
+  return redirect("/");
 };
