f5c974a821a707dacd0f41f02be116e26503007d
Nowe-melancholy <jackpot88230021@gmail.com>
2024-12-01 22:40:20 +0900

    toast

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 } from '@remix-run/react';
 
 import './tailwind.css';
+import { Toaster } from './components/ui/toaster';
 
 export const links: LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -34,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Toaster />
         <ScrollRestoration />
         <Scripts />
       </body>
